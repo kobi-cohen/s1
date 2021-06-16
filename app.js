0775336bd40a82a12b16613da0aab3e59b3808d8
Kobi Cohen <kobicohencom@gmail.com>
2021-06-16 15:49:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
 let port = process.env.PORT || 3000;
 
 console.log('the server is running :)')
